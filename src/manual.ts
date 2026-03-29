c3004898f23583b226a54ce974b46db276517041
Memo Chou <memochou1993@gmail.com>
2026-03-29 15:56:50 +0800

    Update package.json

--- src/manual.ts
+++ src/manual.ts
@@ -41,13 +41,13 @@ const initMivaBDN = ({
 
     switch (data?.status) {
       case 'ready': {
-        console.log('Received ready message from Miva iframe');
+        console.debug('Received ready message from Miva iframe');
         const payload = { status: 'acknowledged' };
         iframe.contentWindow?.postMessage(payload, targetOrigin);
         break;
       }
       case 'confirmed': {
-        console.log('Received confirmed message from Miva iframe');
+        console.debug('Received confirmed message from Miva iframe');
         break;
       }
     }
